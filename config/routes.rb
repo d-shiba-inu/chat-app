3172c09f0dbe924db0d028861e8f408c57c7523c
d-shiba-inu <daikiclassic8@gmail.com>
2026-01-12 18:04:22 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  get "up" => "rails/health#show", as: :rails_health_check
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
